b42291e77c6aaf0de0c6c4192b6aa2f54c42bdcc
rahulkumar-iOS-dev <rahulk@dewsolutions.in>
2024-04-01 16:00:49 +0530

    Added Example App

---
+++ CountryCodePicketWithExampleApp/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Rahul Kumar on 01/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
