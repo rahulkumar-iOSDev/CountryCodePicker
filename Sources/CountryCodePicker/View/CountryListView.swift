eca49c4216f229c38ffe2eab6bdf5eb1ffe77d0e
rahulkumar-iOS-dev <rahulk@dewsolutions.in>
2024-04-01 12:15:20 +0530

    Revert "Added Example App"
    This reverts commit 865ecf2848d6a3f6b72d4a530c1727ab05886d2d.

--- Sources/CountryCodePicker/View/CountryListView.swift
+++ Sources/CountryCodePicker/View/CountryListView.swift
@@ -12,17 +12,14 @@ public struct CountryListView: View {
     @Binding public var isCountryListPresnted: Bool
     @StateObject private var viewModel = CountryListViewModel()
     public var onTapOfCountryRow: ((Country) -> Void)
-    public var testingClosure: ((Int) -> Bool)
     
-    public init(isCountryListPresnted: Binding<Bool>, onTapOfCountryRow: @escaping ((Country) -> Void), testingClosure: @escaping ((Int) -> Bool)) {
+    public init(isCountryListPresnted: Binding<Bool>, onTapOfCountryRow: @escaping ((Country) -> Void)) {
         _isCountryListPresnted = isCountryListPresnted
         self.onTapOfCountryRow = onTapOfCountryRow
-        self.testingClosure = testingClosure
         
     }
     
     public var body: some View {
-        
         NavigationStack {
             VStack {
                 if viewModel.filteredCountries.isEmpty {
@@ -44,7 +41,6 @@ public struct CountryListView: View {
                             .frame(height: 28)
                             .listRowSeparator(.hidden)
                             .onTapGesture {
-                                print(testingClosure(6))
                                 onTapOfCountryRow(country)
                                 isCountryListPresnted = false
                                 
@@ -72,8 +68,6 @@ public struct CountryListView: View {
 }
 
 #Preview {
-    CountryListView(isCountryListPresnted: .constant(false), onTapOfCountryRow: { _ in }, testingClosure: { _ in
-        return false
-    })
+    CountryListView(isCountryListPresnted: .constant(false), onTapOfCountryRow: { _ in })
 }
 
