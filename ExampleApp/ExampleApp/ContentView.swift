92d5a84d209823fd4c77356c656e5d14fe738103
rahulkumar-iOS-dev <rahulk@dewsolutions.in>
2024-04-01 16:21:41 +0530

    Added Example App

---
+++ ExampleApp/ExampleApp/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  ExampleApp
+//
+//  Created by Rahul Kumar on 01/04/24.
+//
+
+import SwiftUI
+import CountryCodePicker
+
+struct ContentView: View {
+    
+    @State var isCountryListPresnted: Bool = false
+    var body: some View {
+
+        Button {
+            isCountryListPresnted = true
+        } label: {
+            Text("Show Country Picker")
+        }
+        
+        .fullScreenCover(isPresented: $isCountryListPresnted, content: {
+            CountryListView(isCountryListPresnted: $isCountryListPresnted) { country in
+                print(country)
+            }
+        })
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExampleApp/ExampleApp/ExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleAppApp.swift
+//  ExampleApp
+//
+//  Created by Rahul Kumar on 01/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
